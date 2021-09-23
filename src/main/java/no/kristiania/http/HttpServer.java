a766d3cf2dfab738b3e3fde85add0fa3c18b6de8
leaskagen <50326406+leaskagen@users.noreply.github.com>
2021-09-23 14:44:44 +0200

    Simple server

--- src/main/java/no/kristiania/http/HttpServer.java
+++ src/main/java/no/kristiania/http/HttpServer.java
@@ -1,4 +1,26 @@
 package no.kristiania.http;
 
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+
 public class HttpServer {
+
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(6969);
+
+        Socket clientSocket = serverSocket.accept();
+
+        String html = "Hello World";
+
+        String response = "HTTP/1.1 200 Morrapuler\r\n" +
+                "Content-length: " + html.length() + "\r\n" +
+                "Connection: close \r\n" +
+                "\r\n" +
+                html;
+
+        clientSocket.getOutputStream().write(response.getBytes());
+    }
 }
